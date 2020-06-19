a32cd851949d42ded264670e4c9a2c321046731c
Mahmoud Tabikh <mahmoud.tabikh@hotmail.com>
2020-06-19 09:39:55 +0200

    dunno what this is

--- initial code.py
+++ initial code.py
@@ -1,5 +1,18 @@
-print ('hello world')
-
-print('this is the second line. very creative, i know.')
-
-print('this is the third line')
+# Python version
+import sys
+print('Python: {}'.format(sys.version))
+# scipy
+import scipy
+print('scipy: {}'.format(scipy.__version__))
+# numpy
+import numpy
+print('numpy: {}'.format(numpy.__version__))
+# matplotlib
+import matplotlib
+print('matplotlib: {}'.format(matplotlib.__version__))
+# pandas
+import pandas
+print('pandas: {}'.format(pandas.__version__))
+# scikit-learn
+import sklearn
+print('sklearn: {}'.format(sklearn.__version__))
